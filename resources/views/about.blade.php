f22ce10096e01881497927b5246a247029172ccd
eldadmamo <eldadf456@gmail.com>
2025-08-02 16:19:48 +0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <p>This is the about page of the controllers.</p> 
+    <a href="{{route('contact.page')}}">Contact Us</a>      
+    </body>
+</html> 
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html>
+<head>      
+    <title>Contact Page</title>
+</head>     
+<body>
+    <h2>Contact Us</h2>
+    <a href="{{url('/about')}}">About Us</a>
+</body>
+</html>
